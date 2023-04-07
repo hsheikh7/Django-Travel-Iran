6df5b7445caecdb03a6145383174cf70f59b61f3
hsheikh7 <hassan.sheikh85@gmail.com>
2023-04-07 21:21:18 +0430

    Created Category
    Created Category class
    + added category field to Post class

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
+
 
 class PostAdmin (admin.ModelAdmin):
     date_hierarchy = 'created_date' 
@@ -9,4 +10,6 @@ class PostAdmin (admin.ModelAdmin):
     list_display = ('title', 'author', 'counted_view', 'status', 'publish_date', 'created_date', )
     list_filter = ('status','author',)
     search_fields = ('title', 'content',)
+
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)

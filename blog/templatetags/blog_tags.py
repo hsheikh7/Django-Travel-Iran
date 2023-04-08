9e155ead8ab29b516ac17b9db348b2a19828645b
hsheikh7 <hassan.sheikh85@gmail.com>
2023-04-08 12:49:58 +0430

    Created categories of post in sidebar
    Post Categories shows number of posts in each category

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library() 
 
@@ -9,7 +10,17 @@ def function():
     return posts 
 
 
-@register.inclusion_tag('latest-posts.html')
+@register.inclusion_tag('blog/latest-posts.html')
 def latestposts(arg=3):
     posts= Post.objects.filter(status= 1).order_by('-publish_date')[:arg]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status = 1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category= name).count()
+    return {'categories': cat_dict}
